cab2cfa6b1b4912bfef559a7d05952b934c604d3
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-03-13 15:07:34 +0900

    renaming variables

--- app/src/main/java/com/librarydevloperjo/cointracker/data/room/KDataDAO.kt
+++ app/src/main/java/com/librarydevloperjo/cointracker/data/room/KDataDAO.kt
@@ -7,13 +7,13 @@ interface KDataDAO {
     @Query("SELECT * FROM kdata")
     fun getAllKData():List<KPremiumData>
 
-    @Query("SELECT * FROM kdata WHERE coinName = :coinName")
-    fun searchKData(coinName:String):List<KPremiumData>
+    @Query("SELECT * FROM kdata WHERE ticker = :ticker")
+    fun searchKData(ticker:String):List<KPremiumData>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertKData(kdata: KPremiumData)
 
     //@Delete
-    @Query("DELETE FROM kdata WHERE coinName = :coinName")
-    suspend fun deleteKData(coinName:String)
+    @Query("DELETE FROM kdata WHERE ticker = :ticker")
+    suspend fun deleteKData(ticker:String)
 }
