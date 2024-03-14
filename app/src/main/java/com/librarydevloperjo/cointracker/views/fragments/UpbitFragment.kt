5174cab0f31238e26dd85466b12ea6ab91e4daa2
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-03-14 23:30:33 +0900

    refactoring

--- app/src/main/java/com/librarydevloperjo/cointracker/views/fragments/UpbitFragment.kt
+++ app/src/main/java/com/librarydevloperjo/cointracker/views/fragments/UpbitFragment.kt
@@ -6,22 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.activityViewModels
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.librarydevloperjo.cointracker.R
 import com.librarydevloperjo.cointracker.adapters.UpbitAdapter
-import com.librarydevloperjo.cointracker.data.gson.UpbitCoin
 import com.librarydevloperjo.cointracker.databinding.FragmentUpbitBinding
-import com.librarydevloperjo.cointracker.util.LOCALE_KEY
-import com.librarydevloperjo.cointracker.util.LOCALE_KOREAN
+import com.librarydevloperjo.cointracker.util.CHANGE_RATE_ASCENDING
+import com.librarydevloperjo.cointracker.util.CHANGE_RATE_DESCENDING
+import com.librarydevloperjo.cointracker.util.PRICE_ASCENDING
+import com.librarydevloperjo.cointracker.util.PRICE_DESCENDING
 import com.librarydevloperjo.cointracker.util.PreferenceManager
 import com.librarydevloperjo.cointracker.viewmodels.CoinsViewModel
 import com.librarydevloperjo.cointracker.viewmodels.UIViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -29,9 +24,8 @@ class UpbitFragment : Fragment() {
 
     private var _binding:FragmentUpbitBinding? = null
     private val binding get() = _binding!!
-    private val viewmodel: CoinsViewModel by activityViewModels()
-    private val uiViewmodel: UIViewModel by activityViewModels()
-    private val adapter = UpbitAdapter()
+    private val viewModel: CoinsViewModel by activityViewModels()
+    private val uiViewModel: UIViewModel by activityViewModels()
     @Inject
     lateinit var prefrence:PreferenceManager
 
@@ -44,43 +38,45 @@ class UpbitFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View? {
         _binding = FragmentUpbitBinding.inflate(inflater, container, false)
+        val adapter = UpbitAdapter(prefrence)
         binding.apply {
             rvUpbit.layoutManager = LinearLayoutManager(requireActivity())
             rvUpbit.adapter = adapter
 
             rootTabprice.setOnClickListener {
-                val sortState = viewmodel.upbitSortState.value!!
-                when(sortState){
-                    PRICE_ASCENDING -> {sortRV(PRICE_DESCENDING, adapter.currentList.sortedByDescending { it.tradePrice })
-                        ivSortprice.setImageResource(R.drawable.sortdescending)
-                        ivSortkp.setImageResource(R.drawable.sort)}
-                    PRICE_DESCENDING -> {sortRV(PRICE_ASCENDING, adapter.currentList.sortedBy { it.tradePrice })
-                        ivSortprice.setImageResource(R.drawable.sortascending)
-                        ivSortkp.setImageResource(R.drawable.sort)}
-                    else -> {sortRV(PRICE_DESCENDING, adapter.currentList.sortedByDescending { it.tradePrice })
-                        ivSortprice.setImageResource(R.drawable.sortdescending)
-                        ivSortkp.setImageResource(R.drawable.sort)}
+                when(viewModel.upbitSortState.value!!){
+                    PRICE_ASCENDING -> {
+                        sortPriceByDesc = true
+                        sortChangeRateByDesc = null
+                        viewModel.upbitSortState.value = PRICE_DESCENDING
+                    }
+                    else -> {
+                        sortPriceByDesc = false
+                        sortChangeRateByDesc = null
+                        viewModel.upbitSortState.value = PRICE_ASCENDING
+                    }
                 }
             }
             rootTabChangerate.setOnClickListener {
-                val sortState = viewmodel.upbitSortState.value!!
-                when(sortState){
-                    CHANGE_RATE_ASCENDING -> {sortRV(CHANGE_RATE_DESCENDING, adapter.currentList.sortedByDescending { it.changeRate })
-                        ivSortkp.setImageResource(R.drawable.sortdescending)
-                        ivSortprice.setImageResource(R.drawable.sort)}
-                    CHANGE_RATE_DESCENDING -> {sortRV(CHANGE_RATE_ASCENDING, adapter.currentList.sortedBy { it.changeRate })
-                        ivSortkp.setImageResource(R.drawable.sortascending)
-                        ivSortprice.setImageResource(R.drawable.sort)}
-                    else -> {sortRV(CHANGE_RATE_DESCENDING, adapter.currentList.sortedByDescending { it.changeRate })
-                        ivSortkp.setImageResource(R.drawable.sortdescending)
-                        ivSortprice.setImageResource(R.drawable.sort)}
+                when(viewModel.upbitSortState.value!!){
+                    CHANGE_RATE_ASCENDING -> {
+                        sortChangeRateByDesc = true
+                        sortPriceByDesc = null
+                        viewModel.upbitSortState.value = CHANGE_RATE_DESCENDING
+                    }
+                    else -> {
+                        sortChangeRateByDesc = false
+                        sortPriceByDesc = null
+                        viewModel.upbitSortState.value = CHANGE_RATE_ASCENDING
+                    }
                 }
             }
+
             btnSetting.setOnClickListener {
-                uiViewmodel.isInfoFragment.value = true
+                uiViewModel.isInfoFragment.value = true
             }
         }
-        subscribeUI()
+        subscribeUI(adapter)
         return binding.root
     }
 
@@ -89,42 +85,18 @@ class UpbitFragment : Fragment() {
         _binding = null
     }
 
-    private fun sortRV(state:Int,sorted:List<UpbitCoin>){
-        viewmodel.upbitSortState.value = state
-        adapter.submitList(sorted){binding.rvUpbit.scrollToPosition(0)}
-    }
+    private fun subscribeUI(adapter: UpbitAdapter){
+        viewModel.upbitList.observe(viewLifecycleOwner){
+            binding.isLoaded = !it.isNullOrEmpty()
+            binding.count = "(${it.size})"
 
-    private fun subscribeUI(){
-        lifecycleScope.launch {
-            repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewmodel.coinPricesTickFlow.collectLatest {
-                    val upbit = it.upbit
-//                    upbit.forEach {
-//                        it. = if(prefrence.getInt(LOCALE_KEY)== LOCALE_KOREAN) it.korean_name else it.english_name
-//                        if(it.textview_name.isNullOrEmpty()) it.textview_name = it.coin.replace("KRW-","")
-//                    }
-                    viewmodel.upbitList.value = upbit
-                }
-            }
+            val sorted = viewModel.sortUpbitByState(viewModel.upbitSortState.value!!, it)
+            adapter.submitList(sorted)
         }
-        viewmodel.upbitList.observe(viewLifecycleOwner){ upbit->
-            binding.isLoaded = !upbit.isNullOrEmpty()
 
-            when(viewmodel.upbitSortState.value){
-                PRICE_ASCENDING -> upbit.sortBy { it.tradePrice }
-                PRICE_DESCENDING -> upbit.sortByDescending { it.tradePrice }
-                CHANGE_RATE_ASCENDING -> upbit.sortBy { it.changeRate }
-                CHANGE_RATE_DESCENDING -> upbit.sortByDescending { it.changeRate }
-            }
-            adapter.submitList(upbit)
-            binding.tvTotalnum.text = "(${upbit.size})"
+        viewModel.upbitSortState.observe(viewLifecycleOwner){
+            val sorted = viewModel.sortUpbitByState(it, viewModel.upbitList.value!!)
+            adapter.submitList(sorted){ binding.rvUpbit.scrollToPosition(0) }
         }
-    }
-
-    companion object {
-        private const val PRICE_ASCENDING = 0
-        private const val PRICE_DESCENDING = 1
-        private const val CHANGE_RATE_ASCENDING = 10
-        private const val CHANGE_RATE_DESCENDING = 11
     }
 }
