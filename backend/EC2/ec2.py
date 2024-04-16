206955bf993cee82913c029b4c5162dac7460de1
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-04-16 23:16:08 +0900

    refactor function names

--- backend/EC2/ec2.py
+++ backend/EC2/ec2.py
@@ -14,14 +14,14 @@ class Binance:
         self.base_endpoint = 'https://api.binance.com'
         self.api_interval = 60
 
-    def getCurrentPrice(self, ticker):
+    def get_current_price(self, ticker):
         url = self.base_endpoint + "/api/v3/ticker/price"
         param = {
             "symbol":ticker
         }
         return requests.get(url,params=param).json()
     
-    def getTickers(self):
+    def get_tickers(self):
         url = self.base_endpoint + "/api/v3/exchangeInfo"
         full_data = requests.get(url).json()['symbols']
         return {s['symbol']: s for s in full_data if 'BUSD' in s['symbol'] and not 'USDT' in s['symbol'] and not 'USDC' in s['symbol'] 
@@ -33,7 +33,7 @@ class Upbit:
         self.base_endpoint = 'https://api.upbit.com'
         self.api_interval = 60
 
-    def getCurrentPrice(self, ticker):
+    def get_current_price(self, ticker):
         url = self.base_endpoint + "/v1/ticker"
         header = {"Accept": "application/json"}
         param = {
@@ -41,14 +41,14 @@ class Upbit:
         }
         return requests.get(url,params=param ,headers=header).json()[0]
     
-    def getTickers(self):
+    def get_tickers(self):
         url = self.base_endpoint + "v1/market/all?isDetails=false"
         headers = {"Accept": "application/json"}
-        fulldata = requests.get(url,headers=headers).json()
-        for f in fulldata: 
+        full_data = requests.get(url,headers=headers).json()
+        for f in full_data: 
             if(f['market']=='KRW-BTT'):
                 f['market'] = 'KRW-BTTC' # BTTC problem , see here : https://newsroompost.com/business/what-is-bttc-and-what-happened-to-btt-how-to-swap-bttold-read-here/5057305.html
-        return {s['market']: s for s in fulldata if 'KRW' in s['market']}
+        return {s['market']: s for s in full_data if 'KRW' in s['market']}
     
 
 class ExchangeRate:
@@ -56,17 +56,16 @@ class ExchangeRate:
         self.base_endpoint = 'https://quotation-api-cdn.dunamu.com/v1/forex/recent'
         self.api_interval = 60*60*12
 
-    def getExchangeRate():
-        url = 'https://quotation-api-cdn.dunamu.com/v1/forex/recent'
+    def get_exchange_rate(self):
         param = {
             "codes" : "FRX.KRWUSD",
         }
-        return requests.get(url=url,params=param).json()    
+        return requests.get(url=self.base_endpoint,params=param).json()
 
 
 class MongoDB:
     def __init__(self) -> None:
-        self.password = 'your_password_for_mongodb_cloud'
+        self.password = 'your_password_for_mongodb_cloud' # os.getenv('MONGO_DB_PASSWORD')
         self.connection_url = f'mongodb+srv://your_id_for_mongodb_cloud:{self.password}@cluster0.gulri.mongodb.net/MyDatabase?retryWrites=true&w=majority'
         self.client = MongoClient(self.connection_url)
         self.db = self.client['Coins']
@@ -75,22 +74,22 @@ class MongoDB:
         self.exchange_rate = self.db['ExchangeRate']
         self.api_interval = 0.01
 
-    def renewUpbitPrices(self, upbit:Upbit):
-        upbit_coins = upbit.getTickers()
+    def renew_upbit_prices(self, upbit:Upbit):
+        upbit_coins = upbit.get_tickers()
         for c in upbit_coins:
-            price = upbit.getCurrentPrice(c)
+            price = upbit.get_current_price(c)
             self.upbit.update_many({"coin":f"{c}"},{'$set':price},upsert=True)
             time.sleep(self.api_interval)
 
-    def renewBinancePrices(self, binance:Binance):
-        binance_coins = binance.getTickers()
+    def renew_binance_prices(self, binance:Binance):
+        binance_coins = binance.get_tickers()
         for c in binance_coins:
-            price = binance.getCurrentPrice(c)
+            price = binance.get_current_price(c)
             self.binance.update_many({"coin":f"{c}"},{'$set':price},upsert=True)
             time.sleep(self.api_interval)
 
-    def renewExchangeRate(self, exchange_rate:ExchangeRate):
-        data = exchange_rate.getExchangeRate()[0]
+    def renew_exchange_rate(self, exchange_rate:ExchangeRate):
+        data = exchange_rate.get_exchange_rate()[0]
         self.exchange_rate.update_many({"data":data['currencyCode']},{'$set':data},upsert=True)
 
 if __name__ == "__main__":
@@ -99,18 +98,18 @@ if __name__ == "__main__":
     exchange_rate = ExchangeRate()
     mongodb = MongoDB()
 
-    def job_mongodb_renewUpbitPrices():
-        mongodb.renewUpbitPrices(upbit=upbit)
+    def job_mongodb_renew_upbit_prices():
+        mongodb.renew_upbit_prices(upbit=upbit)
 
-    def job_mongodb_renewBinancePrices():
-        mongodb.renewBinancePrices(binance=binance)
+    def job_mongodb_renew_binance_prices():
+        mongodb.renew_binance_prices(binance=binance)
 
-    def job_mongodb_renewExchangeRate():
-        mongodb.renewExchangeRate(exchange_rate=exchange_rate)
+    def job_mongodb_renew_exchange_rate():
+        mongodb.renew_exchange_rate(exchange_rate=exchange_rate)
     
-    schedule.every(upbit.api_interval).minutes.do(job_mongodb_renewUpbitPrices)
-    schedule.every(binance.api_interval).minutes.do(job_mongodb_renewBinancePrices)
-    schedule.every(exchange_rate.api_interval).hours.do(job_mongodb_renewExchangeRate)
+    schedule.every(upbit.api_interval).minutes.do(job_mongodb_renew_upbit_prices)
+    schedule.every(binance.api_interval).minutes.do(job_mongodb_renew_binance_prices)
+    schedule.every(exchange_rate.api_interval).hours.do(job_mongodb_renew_exchange_rate)
 
     while True:
         schedule.run_pending()
