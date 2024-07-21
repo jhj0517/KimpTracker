baa9bba7a9b22cd9ae1828d263850bbfc243b7bf
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-07-22 00:14:58 +0900

    Add base abstractmethod functions

--- backend/EC2/markets/crypto_currency_market_base.py
+++ backend/EC2/markets/crypto_currency_market_base.py
@@ -1,18 +1,33 @@
-from abc import ABC
+from abc import ABC, abstractmethod, abstractproperty
+from typing import Optional, List
 
 
 class CryptoCurrencyMarketBase(ABC):
-    def __init__(self):
-        self.base_endpoint = None
-        self.rpm = None
+    def __init__(self,
+                 base_endpoint: str,
+                 rpm: Optional[int] = None):
+        self.base_endpoint = base_endpoint
+        self.rpm = rpm
         self.api_interval = None
+        if self.rpm is not None:
+            self.api_interval = 1 / (self.rpm / 60)
 
-    @property
-    def rpm(self):
-        return self.rpm
+    @abstractmethod
+    def get_currencies(self) -> List:
+        """
+        Get all currency codes from the platform
 
-    @rpm.setter
-    def rpm(self, value):
-        self.rpm = value
-        if value is not None:
-            self.api_interval = 1 / (value / 60)
+        :return: List of currency codes
+        """
+        pass
+
+    @abstractmethod
+    def get_current_price(self,
+                          currency: str):
+        """
+        Get the latest price of the currency from the platform
+
+        :param currency: Currency code
+        :return: Price info data of the currency code from the platform
+        """
+        pass
