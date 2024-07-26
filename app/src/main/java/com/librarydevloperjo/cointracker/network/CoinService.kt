c83f8139573483c1ba39e8875caa9e64de59021a
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-07-26 13:28:16 +0900

    update endpoints in retrofit

--- app/src/main/java/com/librarydevloperjo/cointracker/network/CoinService.kt
+++ app/src/main/java/com/librarydevloperjo/cointracker/network/CoinService.kt
@@ -2,6 +2,10 @@ package com.librarydevloperjo.cointracker.network
 
 import com.librarydevloperjo.cointracker.BuildConfig
 import com.librarydevloperjo.cointracker.data.gson.BaseResponse
+import com.librarydevloperjo.cointracker.data.gson.BinanceResponse
+import com.librarydevloperjo.cointracker.data.gson.ExchangeRateResponse
+import com.librarydevloperjo.cointracker.data.gson.KimchiPremiumResponse
+import com.librarydevloperjo.cointracker.data.gson.UpbitResponse
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -10,8 +14,17 @@ import retrofit2.http.GET
 
 interface CoinService {
 
-    @GET("price")
-    suspend fun getPlatformData(): BaseResponse
+    @GET("/binance")
+    suspend fun fetchBinanceData(): BinanceResponse
+
+    @GET("/upbit")
+    suspend fun fetchUpbitData(): UpbitResponse
+
+    @GET("/exchange-rate")
+    suspend fun fetchExchangeRateData(): ExchangeRateResponse
+
+    @GET("/kimchi-premium")
+    suspend fun fetchKimchiPremiumData(): KimchiPremiumResponse
 
     companion object {
         private const val BASE_URL = BuildConfig.BaseURL
