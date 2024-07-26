a7392e3992274c4df6f2326e404bb3f824646752
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-07-26 20:01:45 +0900

    rename variable

--- app/src/main/java/com/librarydevloperjo/cointracker/viewmodels/BinanceAdapterViewModel.kt
+++ app/src/main/java/com/librarydevloperjo/cointracker/viewmodels/BinanceAdapterViewModel.kt
@@ -1,20 +1,20 @@
 package com.librarydevloperjo.cointracker.viewmodels
 
-import com.librarydevloperjo.cointracker.data.gson.BinanceItem
+import com.librarydevloperjo.cointracker.data.gson.KimchiPremiumItem
 import java.math.BigDecimal
 import java.text.NumberFormat
 import java.util.Locale
 
 class BinanceAdapterViewModel(
-    private val item: BinanceItem
+    private val item: KimchiPremiumItem
 ) {
     val name:String
-        get() = item.symbol
+        get() = item.binanceData.symbol
 
-    val price:String
-        get() = formatPrice(item.price)
+    val priceText:String
+        get() = formatPrice(item.binanceData.price)
 
-    private fun formatPrice(price: String): String {
+    private fun formatPrice(price: Double): String {
         val bigDecimal = BigDecimal(price)
         val stripped = bigDecimal.stripTrailingZeros()
         val formatted = nFormat.format(stripped)
