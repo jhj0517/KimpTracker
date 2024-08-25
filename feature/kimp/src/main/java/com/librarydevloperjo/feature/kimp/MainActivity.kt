93f6379b02aec247a80157e152551f321dfa2973
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-08-26 00:25:17 +0900

    Add feature modules

---
+++ feature/kimp/src/main/java/com/librarydevloperjo/feature/kimp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.librarydevloperjo.feature.kimp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
