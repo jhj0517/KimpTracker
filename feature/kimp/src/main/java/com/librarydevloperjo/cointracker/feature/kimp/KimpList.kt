ebac4c2f3f55748439c43d96c08f5119a804594c
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2024-08-28 00:37:17 +0900

    Test preview

--- feature/kimp/src/main/java/com/librarydevloperjo/cointracker/feature/kimp/KimpList.kt
+++ feature/kimp/src/main/java/com/librarydevloperjo/cointracker/feature/kimp/KimpList.kt
@@ -2,6 +2,7 @@ package com.librarydevloperjo.cointracker.feature.kimp
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,7 +12,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.BasicText
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
