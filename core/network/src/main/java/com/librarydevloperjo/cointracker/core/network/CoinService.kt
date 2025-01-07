42690b7c5d1adf2546e0f4bbdc2484d5fd233e53
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2025-01-08 01:18:53 +0900

    Add network module

---
+++ core/network/src/main/java/com/librarydevloperjo/cointracker/core/network/CoinService.kt
@@ -0,0 +1,48 @@
+package com.librarydevloperjo.cointracker.core.network
+
+
+import com.librarydevloperjo.cointracker.BuildConfig
+import com.librarydevloperjo.cointracker.data.gson.BinanceResponse
+import com.librarydevloperjo.cointracker.data.gson.ExchangeRateResponse
+import com.librarydevloperjo.cointracker.data.gson.KimchiPremiumItem
+import com.librarydevloperjo.cointracker.data.gson.UpbitResponse
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+interface CoinService {
+
+    @GET("/binance")
+    suspend fun fetchBinanceData(): BinanceResponse
+
+    @GET("/upbit")
+    suspend fun fetchUpbitData(): UpbitResponse
+
+    @GET("/exchange-rate")
+    suspend fun fetchExchangeRateData(): ExchangeRateResponse
+
+    @GET("/kimchi-premium")
+    suspend fun fetchKimchiPremiumData(): List<KimchiPremiumItem>
+
+    companion object {
+        private const val BASE_URL = BuildConfig.BaseURL
+
+        fun create(): CoinService {
+            val logger = HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BASIC }
+
+            val client = OkHttpClient.Builder()
+                .addInterceptor(logger)
+                .build()
+
+            return Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(CoinService::class.java)
+        }
+    }
+
+}
