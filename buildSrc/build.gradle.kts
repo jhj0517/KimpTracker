1da3f6a919870ac2fe4c4a9dd68fb8ceac11b4d3
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2025-01-09 21:16:37 +0900

    Fix typo

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -7,7 +7,7 @@ repositories {
     mavenCentral()
 }
 
-depedencies {
+dependencies {
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
     implementation("com.android.tools.build:gradle:8.1.1")
 }
