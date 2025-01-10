2f1f456011b9e10e0993cfcf0c515a44111c0c25
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2025-01-10 22:20:52 +0900

    Update version

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,9 +1,9 @@
 object Versions {
-    const val compose = "1.4.3"
-    const val composeMaterial3 = "1.1.1"
-    const val composeCompiler = "1.4.6"
-    const val hilt = "2.45"
+    const val compose = "1.7.0-beta07"
+    const val composeMaterial3 = "1.3.0-beta05"
+    const val composeCompiler = "2.0.01"
+    const val hilt = "2.52"
     const val okHttp = "5.0.0-alpha.2"
-    const val retrofit = "2.9.0"
-    const val room = "2.5.0"
+    const val retrofit = "4.12.0"
+    const val room = "2.6.1"
 }
