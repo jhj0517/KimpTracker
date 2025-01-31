0d216aeb167b5b4882bb1d0659fb00b2695dcda3
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2025-02-01 01:51:30 +0900

    Use `LibraryExtension`

--- build-logic/convention/src/main/kotlin/AndroidComposeConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidComposeConventionPlugin.kt
@@ -1,9 +1,9 @@
 import com.android.build.api.dsl.CommonExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
-import org.gradle.kotlin.dsl.withType
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import com.android.build.gradle.LibraryExtension
 
 class AndroidComposeConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
@@ -13,9 +13,18 @@ class AndroidComposeConventionPlugin : Plugin<Project> {
                 apply("org.jetbrains.kotlin.android")
             }
 
+            extensions.configure<LibraryExtension> {
+                buildFeatures {
+                    compose = true
+                }
+
+                composeOptions {
+                    kotlinCompilerExtensionVersion = "1.5.8"
+                }
+            }
+
             dependencies {
-                val bom = libs.findLibrary("androidx.compose.bom").get()
-                add("implementation", platform(bom))
+                add("implementation", platform(libs.findLibrary("androidx-compose-bom").get()))
                 add("implementation", libs.findLibrary("androidx.compose.material3").get())
                 add("implementation", libs.findLibrary("androidx.compose.ui").get())
                 add("implementation", libs.findLibrary("androidx.compose.ui.tooling.preview").get())
