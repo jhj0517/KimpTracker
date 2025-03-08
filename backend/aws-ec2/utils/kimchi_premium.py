a6b0968c9780bea0fc4ad931259f034d304c38db
jhj0517 <97279763+jhj0517@users.noreply.github.com>
2025-03-08 22:05:27 +0900

    Use sentinel value for exception case

--- backend/aws-ec2/utils/kimchi_premium.py
+++ backend/aws-ec2/utils/kimchi_premium.py
@@ -5,6 +5,8 @@ def calculate_kimchi_premium(
 ):
     binance_price_krw = binance_price * exchange_rate
 
+    if binance_price_krw == 0:
+        return -1  # Sentinel value
     kimchi_premium = ((upbit_price - binance_price_krw) / binance_price_krw) * 100
 
     return kimchi_premium
